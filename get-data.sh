4ac761ea32e463da6aa24b570c63f91fff305da8
Eduard Cuba <cuba.edo@gmail.com>
2020-12-05 23:46:34 +0100

    Add harris detector

--- get-data.sh
+++ get-data.sh
@@ -4,8 +4,13 @@
 
 mkdir -p data
 cd data
+rm -rf parking
 curl http://rpg.ifi.uzh.ch/docs/teaching/2016/parking.zip --output parking.zip
-unzip parking.zip
+unzip -q parking.zip
 rm parking.zip
 
 # TODO: remove commas from K.txt
+
+cd parking
+mv K.txt K.csv
+cat K.csv | sed "s/,//g" > K.txt
