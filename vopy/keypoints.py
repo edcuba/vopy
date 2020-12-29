Fix plotting without rotation

vopy/keypoints.py
@@ -69,6 +69,6 @@ def match_descriptors(query_desc, database_desc, match_lambda=4):
 
 def transform_coords(db, query):
     n = db.shape[0]
-    p0 = np.hstack((db, np.ones(n)[:, np.newaxis]))
-    p1 = np.hstack((query, np.ones(n)[:, np.newaxis]))
+    p0 = np.hstack((db, np.ones(n)[:, np.newaxis])).T
+    p1 = np.hstack((query, np.ones(n)[:, np.newaxis])).T
     return p0, p1

vopy/plotting.py
@@ -20,9 +20,23 @@ def plot_frame_shift(ax, im, query_inliers, db_inliers):
     # show their paths in the new image
     ax.plot(x, y, "g-")
 
-def plot_landmarks(ax, P):
+def plot_landmarks(ax, P, pose_history):
     ax.clear()
-    ax.scatter(P[0,:], P[2,:])
+    start_pose = max(0, len(pose_history) - 20)
+    loc = np.array((0., 0., 0.))
+    path_x = [0.]
+    path_z = [0.]
+    for R, T in pose_history[start_pose:]:
+        shift = R.T.dot(T)
+        loc += shift
+        path_x.append(loc[1])
+        path_z.append(loc[2])
+
+    ax.scatter(P[1,:] - loc[1], P[2,:] - loc[2], marker='o', facecolors='none', edgecolors='b')
+    ax.plot(path_x, path_z, "rx")
+    ax.set_ylim(ymin=-20, ymax=20)
+    ax.set_xlim(xmin=-20, xmax=20)
+    # TODO: this needs to be adjusted for rotation, as we are moving to the side, everything is rotated by 90 degs
 
 def plot_camera_pose(ax, pose_history):
     ax.clear()
@@ -31,9 +45,10 @@ def plot_camera_pose(ax, pose_history):
     path_z = [0.]
 
     for R, T in pose_history:
-        shift = -R.T.dot(T)
+        shift = R.T.dot(T)
         loc += shift
-        path_x.append(loc[0])
+        path_x.append(loc[1])
         path_z.append(loc[2])
 
     ax.plot(path_x, path_z)
+    return loc

vopy/pose.py
@@ -37,23 +37,28 @@ def decompose_essential_matrix(E):
     R = np.array((R1, R2))
     return R, u3
 
-
 def cross_matrix(x):
-    return np.array(((0, -x[2], x[1]), (x[2], 0, -x[0]), (-x[1], x[0], 0)))
-
+    return np.array(
+        ((0., -x[2], x[1]),
+        (x[2], 0., -x[0]),
+        (-x[1], x[0], 0.)),
+        dtype=np.float64
+    )
 
 def triangulate(points0, points1, M1, M2):
-    num_points = points0.shape[0]
+    num_points = points0.shape[1]
     P = np.zeros((4, num_points))
-    for p0, p1, i in zip(points0, points1, range(num_points)):
-        A1 = cross_matrix(p0).dot(M1)
-        A2 = cross_matrix(p1).dot(M2)
+    for i in range(num_points):
+        p0 = points0[:, i]
+        p1 = points1[:, i]
+        c0 = cross_matrix(p0)
+        c1 = cross_matrix(p1)
+        A1 = c0.dot(M1)
+        A2 = c1.dot(M2)
         A = np.vstack((A1, A2))
         _, _, V = np.linalg.svd(A)
-        h = V[:,-1]
-        P[:,i] = h / h[-1]
-    return P
-
+        P[:,i] = V[:,-1]
+    return P / np.tile(P[-1,:], (4, 1))
 
 def disambiguate_poses(rots, u3, points0, points1, K):
     M1 = K.dot(np.eye(3, 4))